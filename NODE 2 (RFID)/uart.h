431e7c466ecc5b9c7e0f0600de41258d9eebcd0d
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-11-28 22:34:28 +0530

    UART for RFID

---
+++ NODE 2 (RFID)/uart.h
@@ -0,0 +1,25 @@
+void UART0_CONFIGURE()
+{
+PINSEL0 |=0X05;  // PO.0,PO.1    TX & RX 
+U0LCR=0X83;
+//(60000000)/(16*9600)	=>390 
+U0DLL=134;		   //134
+U0DLM=1;	 //1
+U0LCR=0X3;
+}
+
+void UART0_TX(unsigned char data)
+{
+while(((U0LSR>>5)&1)==0);
+U0THR=data;
+}
+
+
+
+void uart_str(char *s)
+{
+while(*s)
+{
+UART0_TX(*s++);
+}
+}
