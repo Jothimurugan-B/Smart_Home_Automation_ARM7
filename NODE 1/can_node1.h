7f44158a4ba59b0905952bbc433d9fa12e7b4d35
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-11-28 22:03:34 +0530

    CAN code for node 1(IR sensor)

--- NODE 1/can_node1.h
+++ NODE 1/can_node1.h
@@ -1,59 +1,39 @@
-#include<lpc21xx.h>
-
+#include <lpc21xx.h>
 
 void can_init(void);
-
 void can_tx(can_msg);
 
-
-
-
 void can_init(void)
-
 {
+    PINSEL1 |= 0X00014000;
+    VPBDIV = 1; //60 MHz
 
-  PINSEL1|=0X00014000;
+    C2MOD = 0X01; //Reset mode
+    AFMR = 0X02;
+    C2BTR = 0X001C001D; //125 kbs @ 60 MHz
 
-	VPBDIV=1; //60 MHz
-
-	
-
-	C2MOD = 0X01; //Reset mode
-
-	AFMR = 0X02;
-	C2BTR= 0X001C001D; //125 kbs @ 60 mhz
-
-
-	C2MOD=0X00; //normal mode
-
+    C2MOD = 0X00; //Normal mode
 }
-
 
 void can_tx(can_msg m1)
 {
-	C2TID1 = m1.msgid;
-	C2TFI1 = (m1.dlc<<16);
+    C2TID1 = m1.msgid;
+    C2TFI1 = (m1.dlc << 16);
 
-	if(m1.rtr==0)
-	{
-	  C2TFI1 &= ~(m1.rtr<<30);
+    if (m1.rtr == 0)
+    {
+        C2TFI1 &= ~(m1.rtr << 30);
+        C2TDA1 = m1.byteA;
+        C2TDB1 = m1.byteB;
+    }
+    else
+    {
+        C2TFI1 |= (m1.rtr << 30);
+        C2TDA1 = m1.byteA;
+        C2TDB1 = m1.byteB;
+    }
 
-	  C2TDA1 = m1.byteA;
+    C2CMR = (1 << 0) | (1 << 5); //TX Request
 
-	  C2TDB1 = m1.byteB;
-	}
-	else
-	{
-		C2TFI1 |= (m1.rtr<<30);
-
-	  C2TDA1 = m1.byteA;
-
-	  C2TDB1 = m1.byteB;
-	}
-
-	C2CMR = (1<<0) | (1<<5); //TX Request
-
-	
-	while((C2GSR&(1<<3))==0); //wait for txmission
-
+    while ((C2GSR & (1 << 3)) == 0); //Wait for transmission
 }
