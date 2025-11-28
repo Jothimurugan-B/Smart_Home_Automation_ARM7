e398c2f09ce5b08f33f2404a71086cc98902b8e2
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-11-28 21:57:20 +0530

    Add files via upload

---
+++ NODE 1/delay.h
@@ -0,0 +1,113 @@
+#include<LPC21XX.H>
+
+
+
+void delay_sec(int t);
+
+
+
+void delay_ms(int t);
+
+
+
+void delay_microsec(int t);
+
+
+
+
+
+
+void delay_sec(int t)
+
+
+
+{
+
+
+
+  T0PR=60000000-1;
+
+
+
+  T0TCR=0X1;
+
+
+
+  while(T0TC<t);
+
+
+
+  T0TCR=0X3;
+
+
+
+  T0TCR=0X0;
+
+
+
+ }
+
+
+
+void delay_ms(int t)
+
+
+
+{
+
+
+
+  T0PR=60000-1;
+
+
+
+  T0TCR=0X1;
+
+
+
+  while(T0TC<t);
+
+
+
+  T0TCR=0X3;
+
+
+
+  T0TCR=0X0;
+
+
+
+ }
+
+
+
+void delay_microsec(int t)
+
+
+
+{
+
+
+
+  T0PR=60-1;
+
+
+
+  T0TCR=0X1;
+
+
+
+  while(T0TC<t);
+
+
+
+  T0TCR=0X3;
+
+
+
+  T0TCR=0X0;
+
+
+
+ }
+
---
+++ NODE 1/lcd.h
@@ -0,0 +1,111 @@
+#include<LPC21XX.H>
+
+
+#define LCD 0XFF
+
+#define RS 1<<8
+
+#define E 1<<9
+
+
+void lcd_init(void);
+
+void lcd_command(unsigned char);
+
+void lcd_intg(unsigned int);
+void lcd_string(unsigned char*);
+
+void lcd_data(int);
+
+
+
+
+void lcd_init(void)
+
+{
+
+  IODIR0=0X3FF;
+
+  lcd_command(0X01);
+
+  lcd_command(0X02);
+
+  lcd_command(0X0C);
+
+  lcd_command(0X38);
+  lcd_command(0X80);
+
+}
+
+
+void lcd_command(unsigned char cmd)
+
+{
+
+  IOCLR0=LCD;
+
+  IOSET0=cmd;
+
+  IOCLR0=RS;
+
+  IOSET0=E;
+
+  delay_ms(2);
+
+  IOCLR0=E;
+
+}
+
+
+void lcd_intg(unsigned int n)
+
+{
+
+  int i=0,a[10];
+
+  while(n!=0){
+
+  a[i++]=n%10;
+
+  n=n/10;
+
+  }
+
+  for(--i;i>=0;i--)
+
+  lcd_data(a[i]+48);
+
+}
+
+void lcd_string(unsigned char* s)
+
+  {
+
+    int i;
+
+    for(i=0;s[i];i++)
+
+	lcd_data(s[i]);
+
+  }
+
+
+void lcd_data(int d)
+
+{
+
+  IOCLR0=LCD;
+
+  IOSET0=d;
+
+  IOSET0=RS;
+
+  IOSET0=E;
+
+  delay_ms(2);
+
+  IOCLR0=E; 
+
+}
+
+
